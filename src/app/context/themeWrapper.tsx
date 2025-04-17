e4bffd5f34acc2cb2a10975cdd102d5bbd173c5b
the logical guy <Mysstiquee@protonmail.com>
2025-04-17 18:43:21 +0530

    update

--- src/app/context/themeWrapper.tsx
+++ src/app/context/themeWrapper.tsx
@@ -12,22 +12,23 @@ import { mainnet, polygon, optimism, arbitrum, base, sepolia } from "wagmi/chain
 const citreaTestnet = {
   id: 5115,
   name: "Citrea Testnet",
-  network: "Citrea Testnet",
+  network: "citrea-testnet",
   nativeCurrency: {
-    decimals: 18,
-    name: "Citrea",
+    name: "Citrea Test BTC",
     symbol: "cBTC",
+    decimals: 18,
   },
   rpcUrls: {
-    default: {
-      http: ["https://rpc.testnet.citrea.xyz"],
-    },
+    default: { http: ["https://rpc.testnet.citrea.xyz"] },
+    public: { http: ["https://rpc.testnet.citrea.xyz"] },
   },
   blockExplorers: {
-    default: { name: "Citrea Explorer", url: "https://explorer.testnet.citrea.xyz" },
+    default: {
+      name: "Citrea Explorer",
+      url: "https://explorer.testnet.citrea.xyz",
+    },
   },
-  testnet: true,
-} as const;
+};
 
 const config = getDefaultConfig({
   appName: "My RainbowKit App",
